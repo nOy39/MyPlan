21fd88359b366789574d1369616b0407e760e29e
nOy39 <popov.a39rus@gmail.com>
2018-02-19 22:12:35 +0200

    Math.*

---
+++ src/lang/math/AddExact.java
@@ -0,0 +1,11 @@
+package lang.math;
+
+/**
+ * Math.addExact(int a, int b) - Возвращает сумму своих аргументов, вызывая исключение,
+ * если результат переполняет a int.
+ */
+public class AddExact {
+    public static void main(String[] args) {
+        System.out.println(Math.addExact(100,5));
+    }
+}
---
+++ src/lang/math/ConvertDegreesToRad.java
@@ -0,0 +1,11 @@
+package lang.math;
+
+public class ConvertDegreesToRad {
+    public static void main(String[] args) {
+        double degrees = 50;
+        double radiana = 50;
+
+        System.out.println(degrees+" of degrees, "+Math.toRadians(degrees)+" a radians.");
+        System.out.println(radiana+" of radians, "+Math.toDegrees(radiana)+" a degrees.");
+    }
+}
