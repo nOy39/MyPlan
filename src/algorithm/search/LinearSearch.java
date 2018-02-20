d730acc002c59513843e5e3519b8c0b4ca0197c5
nOy39 <popov.a39rus@gmail.com>
2018-02-20 22:52:54 +0200

    JavaDoc

---
+++ src/algorithm/search/LinearSearch.java
@@ -0,0 +1,24 @@
+package algorithm.search;
+
+/**
+ * Created by @author OGI aka nOy39
+ *
+ * @Date 20.02.2018
+ * @Time 22:44
+ */
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] array = {1,4,3,2,6,5,7,8,34,69,15,10,9};
+        System.out.println(linearSearch(array, 348));
+    }
+
+    private static int linearSearch(int[] array, int key) {
+        int i=0;
+        while (key != array[i]) {
+            if (array[i]==key) break;
+            if (i+1>array.length-1) return -1;
+            else i++;
+        }
+        return i;
+    }
+}
